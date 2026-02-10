4a8c5a69b568722fff1de266cb2ec6b465dbea98
cerine0205 <saljahdali0205@outlook.com>
2026-02-10 14:26:47 +0300

    test2

--- Study_Planner/Program.cs
+++ Study_Planner/Program.cs
@@ -10,7 +10,7 @@ namespace Study_Planner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("test");
+            Console.WriteLine("test2");
         }
     }
 }
